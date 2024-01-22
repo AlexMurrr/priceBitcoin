15aeb7b8d6f1edbe340b5406df0ffd7e918216e6
Alex Mur <lehamuraschov2014@yandex.ru>
2024-01-22 19:17:44 +0200

    feat: create navbar and download bootstrap

---
+++ front/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <h1 class="navbar-brand">Price Bitcoin</h1>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Price is now</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">Weekly price chart</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Monthly price chart</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Price chart for the year</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/contact">Choose your range</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+}
+</script>
