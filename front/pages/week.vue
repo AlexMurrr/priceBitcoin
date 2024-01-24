d3b86982fdd6a98cbbd3486e5546b3196ce3a06d
Alex Mur <lehamuraschov2014@yandex.ru>
2024-01-24 21:30:40 +0200

    feat: instoll chart.js

--- front/pages/week.vue
+++ front/pages/week.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>неделя назад</h1>
-
+{{ chart}}
   </div>
 </template>
 
 <script>
 export default {
   name: 'weekPage',
-
+  async asyncData({ $axios }) {
+    try {
+      const response = await $axios.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7');
+      const chart = response.data.prices;
+      return { chart };
+    } catch (error) {
+      console.error('Ошибка при получении данных о цене биткойна:', error);
+    }
+  }
 }
 </script>
