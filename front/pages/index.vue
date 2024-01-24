d61e8b2d8029f1c2ca2ba4f7aad4b80989aeab16
Alex Mur <lehamuraschov2014@yandex.ru>
2024-01-24 14:02:06 +0200

    feat: get price bitcoin now

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
     <h1>сейчас</h1>
+    {{ bitcoinPrice }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async asyncData() {
+    try {
+      const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+      const data = await response.json();
+      const bitcoinPrice = data.bitcoin.usd;
+
+      return {
+        bitcoinPrice
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        bitcoinPrice: null
+      };
+    }
+  }
 }
 </script>
