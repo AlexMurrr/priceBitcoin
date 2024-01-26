01e822ab0b4caad5d089d3e695af945d8357206a
Alex Mur <lehamuraschov2014@yandex.ru>
2024-01-26 22:59:41 +0200

    get price Nano now tamplate

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h1>сейчас</h1>
-   bitoc  {{ bitcoinPrice }}$
+   <p>bitoc  {{ bitcoinPrice }}$</p>
+   <p>nano {{ nanoPrice }}$</p>
   </div>
 </template>
 
@@ -12,9 +13,12 @@ export default {
     try {
       const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
       const data = await response.json();
+      const response1 = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=nano&vs_currencies=usd');
+      const data1 = await response1.json();
       const bitcoinPrice = data.bitcoin.usd;
+      const nanoPrice = data1.nano.usd;
       return {
-        bitcoinPrice
+        bitcoinPrice, nanoPrice
       };
     } catch (error) {
       console.error(error);
